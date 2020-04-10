4cb57b8be1c07f1c7a2f66186210a068e171b4b6
dv00000 <duongvi00000@gmail.com>
2020-04-09 21:31:36 -0400

    Token is hardcoded

--- review_study.js
+++ review_study.js
@@ -90,4 +90,54 @@ function nextPrevReview(n) {
     showTab(currentTab);
     console.log("Update Preview");
     updatePreview();
-  }
+}
+
+function getDetailPending(study_id){
+    var token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODY0ODE3OTUsImV4cCI6MTU5MDgwNTM5NSwic3ViIjoiMTIzNCJ9.bX-XS9h2-8GYocPT8OQgAMK8bNxw41Q0jd6R8Z8S3cs';
+    //Get information from server
+    $(document).ready(function () {
+        $.ajax({
+          url: 'http://pi.cs.oswego.edu:12100/getAdminDetails?study_id='+study_id+'&token='+token,
+          dataType: 'json',
+          success: function (json) {
+            console.log("%j", json);       
+            document.getElementById("title_value").value = json.title; 
+            document.getElementById("reference_value").value = json.references; 
+            document.getElementById("purpose_value").value = json.purpose; 
+            document.getElementById("categories_value").value = json.categories; 
+            document.getElementById("subcategories_value").value = json.subcategories; 
+
+            var keywords = "";
+            for (var i = 0; i < json.keywords.length; i++){
+              if (i!=0){
+                keywords += "; ";
+              }
+              keywords += json.keywords[i];
+            }
+            document.getElementById("keywords_value").value = keywords;  
+            document.getElementById("abstract_value").innerHTML = json.abstract; 
+            document.getElementById("stimuli_value").value = json.num_stimuli; 
+            document.getElementById("duration_value").value = json.duration; 
+            document.getElementById("responses_value").value = json.num_responses; 
+            document.getElementById("trials_value").value = json.num_trials; 
+            if (json.randomize){
+              document.getElementById("randomized_yes_value").checked = true;
+            }
+            else{
+              document.getElementById("randomized_no_value").checked = true;
+            }
+
+            //View list of images and videos
+            var img = json.images;
+            var content = ""
+            for (i = 0; i < img.length; i++) {
+              content += '<div class="preview_img" style="background-image: url(\''+img[i]+'\')"></div>' 
+            }
+            document.getElementById("images_videos").innerHTML = content;
+
+            //View list of json files
+            document.getElementById("json_files").innerHTML = json.template;;
+          }
+        });
+      });   
+}
