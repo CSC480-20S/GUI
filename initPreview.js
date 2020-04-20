c0475cb4ab85bedab648e0eec3f66ff1b7f5f348
Thomas Kressman <58404684+tkressma@users.noreply.github.com>
2020-04-20 14:51:24 -0400

    Update initPreview.js
    Dynamic images.

--- initPreview.js
+++ initPreview.js
@@ -1,89 +1,93 @@
 var token = localStorage['token'];
-
-//Searches the endpoint for the specific study based on the study id and stores the index of the corresponding json object into index.
 $(document).ready(function() {
+  var params = new URLSearchParams(window.location.search.slice());
+  console.log(params.get('id'));
+  var study_id = params.get('id');
+
+  //Calls the endpoint for the specific study based on the study id and returns it's json data.
   $.ajax({
-    url: 'http://pi.cs.oswego.edu:12100/search?token=' + token,
+    url: 'http://pi.cs.oswego.edu:12100/getPreview?study_id=' + study_id + '&token=' + token,
     dataType: 'json',
     success: function(json) {
-      var params = new URLSearchParams(window.location.search.slice());
-      console.log(params.get('id'));
-      var id = params.get('id');
-      var index = 0;
-      for (var i = 0; i < json.length; i++) {
-        if (json[i].studyID == id) {
-          index = i;
-          break;
-        }
-      }
-      
       //Constructs the page and checks if it is in the users wishlist
-      checkWishlistStatus(id);
-      document.getElementById('costInCredits').innerHTML = "Credits: " + json[index].costInCredits;
-      document.getElementById('author').innerHTML = "Author: " + json[index].author;
-      document.getElementById('abst').innerHTML = json[index].abstract;
-      document.getElementById('duration').innerHTML = "Duration of the study: " + json[index].duration + " minutes.";
-      document.getElementById('institution').innerHTML = "College: " + json[index].institution;
-      document.getElementById('title').innerHTML = json[index].title;
-      document.getElementById('purpose').innerHTML = json[index].purpose;
-      document.getElementById('responses').innerHTML = "No. of responses: " + json[index].num_responses;
-      document.getElementById('stimuli').innerHTML = "No. of stimuli: " + json[index].num_stimuli;
-      document.getElementById('studyidPlaceholder').value = id;
+      checkWishlistStatus(study_id);
+      document.getElementById('costInCredits').innerHTML = "Credits: " + json.costInCredits;
+      document.getElementById('author').innerHTML = "Author: " + json.author;
+      document.getElementById('abst').innerHTML = json.abstract;
+      document.getElementById('duration').innerHTML = "Duration of the study: " + json.duration + " minutes.";
+      document.getElementById('institution').innerHTML = "College: " + json.institution;
+      document.getElementById('title').innerHTML = json.title;
+      document.getElementById('purpose').innerHTML = json.purpose;
+      document.getElementById('responses').innerHTML = "No. of responses: " + json.num_responses;
+      document.getElementById('stimuli').innerHTML = "No. of stimuli: " + json.num_stimuli;
+      document.getElementById('studyidPlaceholder').value = study_id;
+
+      var img = json.images;
+      console.log(img);
+      document.getElementById('imagePreview').src = img;
+
       var categories = "Categories: ";
-      for (i = 0; i < json[index].categories.length; i++) {
-        categories += "<button class='pill'>" + json[index].categories[i] + "</button>";
+      for (i = 0; i < json.categories.length; i++) {
+        categories += "<button class='pill'>" + json.categories[i] + "</button>";
       }
       document.getElementById('categories').innerHTML = categories;
+
+      var subcategories = "Subcategories: ";
+      for (i = 0; i < json.subcategories.length; i++) {
+        subcategories += "<button class='pill'>" + json.subcategories[i] + "</button>";
+      }
+      document.getElementById('subcategories').innerHTML = subcategories;
+
       var keywords = "Keywords: ";
-      for (i = 0; i < json[index].keywords.length; i++) {
-        keywords += "<button class='pill'>" + json[index].keywords[i] + "</button>";
+      for (i = 0; i < json.keywords.length; i++) {
+        keywords += "<button class='pill'>" + json.keywords[i] + "</button>";
       }
       document.getElementById('keywords').innerHTML = keywords;
       var stars = "Rating: ";
-      for (i = 0; i < json[index].rating; i++) {
+      for (i = 0; i < json.rating; i++) {
         stars += "<i class='fa fa-star' aria-hidden='true'></i>";
       }
       document.getElementById('stars').innerHTML = stars;
-      if (json[index].randomize) {
+      if (json.randomize) {
         document.getElementById('random').innerHTML = "Randomized the study?: Yes";
       } else {
         document.getElementById('random').innerHTML = "Randomized the study?: No";
       }
-      if (json[index].reviews.length > 1) {
+      if (json.reviews.length > 1) {
         var reviews = '<div class="carousel-item active">' +
           '<div class="card">' +
           '<div class="contain">' +
-          '<h4><b>' + json[index].reviews[0].name + '</b></h4>';
+          '<h4><b>' + json.reviews[0].name + '</b></h4>';
         reviews += '<div class="review"><div class="toCenter">'
-        for (var i = 0; i < json[index].reviews[0].rating; i++) {
+        for (var i = 0; i < json.reviews[0].rating; i++) {
           reviews += "<i class='fa fa-star' aria-hidden='true'></i>";
         }
         reviews += '</div></div>';
-        reviews += "<p>" + json[index].reviews[0].comment + "</p>"
+        reviews += "<p>" + json.reviews[0].comment + "</p>"
         reviews += '</div></div></div>';
-        for (var i = 1; i < json[index].reviews.length; i++) {
+        for (var i = 1; i < json.reviews.length; i++) {
           reviews += '<div class="carousel-item">' +
             '<div class="card">' +
             '<div class="contain">' +
-            '<h4><b>' + json[index].reviews[i].name + '</b></h4>';
+            '<h4><b>' + json.reviews[i].name + '</b></h4>';
           reviews += '<div class="review"><div class="toCenter">'
-          for (var j = 0; j < json[index].reviews[i].rating; j++) {
+          for (var j = 0; j < json.reviews[i].rating; j++) {
             reviews += "<i class='fa fa-star' aria-hidden='true'></i>";
           }
           reviews += '</div></div>';
-          reviews += "<p>" + json[index].reviews[i].comment + "</p>"
+          reviews += "<p>" + json.reviews[i].comment + "</p>"
           reviews += '</div></div></div>';
         }
         reviews += '<div class="carousel-item">' +
           '<div class="card">' +
           '<div class="contain">' +
-          '<h4><b>' + json[index].reviews[0].name + '</b></h4>';
+          '<h4><b>' + json.reviews[0].name + '</b></h4>';
         reviews += '<div class="review"><div class="toCenter">'
-        for (var j = 0; j < json[index].reviews[0].rating; j++) {
+        for (var j = 0; j < json.reviews[0].rating; j++) {
           reviews += "<i class='fa fa-star' aria-hidden='true'></i>";
         }
         reviews += '</div></div>';
-        reviews += "<p>" + json[index].reviews[0].comment + "</p>"
+        reviews += "<p>" + json.reviews[0].comment + "</p>"
         reviews += '</div></div></div>';
         document.getElementById('reviews').innerHTML = reviews;
       }
@@ -92,7 +96,7 @@ $(document).ready(function() {
 });
 
 
-
+//Checks to see if this study is on the users wishlist
 function checkWishlistStatus(id) {
   var feedback_data = '{}';
   const dataToSend = JSON.parse(feedback_data);
