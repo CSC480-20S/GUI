dda0f40d897017415cae1fc3bf092b5b17e34df7
Thomas Kressman <58404684+tkressma@users.noreply.github.com>
2020-04-21 19:24:02 -0400

    Update tokenPrompt.js

--- tokenPrompt.js
+++ tokenPrompt.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   document.getElementById("token_overlay").style.display = "block";
 });
 
+//For index.html. The user enters to token and basically "logs in". Whichever token is used here is saved in localstorage
+//to use across the website.
 function applyToken(token) {
 
   var urlSend = 'http://pi.cs.oswego.edu:12100/checkToken?token=' + token;
