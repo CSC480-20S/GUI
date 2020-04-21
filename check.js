bd3243818e8bf8fdee83851c784a367bcc74a980
Thomas Kressman <58404684+tkressma@users.noreply.github.com>
2020-04-21 19:06:42 -0400

    Update check.js

--- check.js
+++ check.js
@@ -1,3 +1,4 @@
+//Checks to see if an admin marked a study for review. If it is, it changes the icon, and vice versa.
 function changeCheck(x) {
   x.classList.toggle("fa-times-circle-o");
   x.classList.toggle("orange_close");
